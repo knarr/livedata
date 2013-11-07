f540c26d6a46d5213e601b0e27561d83ae4dcf57
emccloskey <erin_mccloskey@brown.edu>
2013-11-07 17:54:19 -0500

    added an adjacency matrix

--- graph/web/main.dart
+++ graph/web/main.dart
@@ -2,5 +2,13 @@ import 'dart:html';
 import 'graph.dart';
 
 void main() {
+
+  List<List> adj = [[1,0,0,1],
+                    [0,1,1,1],
+                    [0,0,1,0],
+                    [1,1,0,0]];                   ]
+  
+  
+  
   
 }
