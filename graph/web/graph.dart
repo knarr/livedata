fac226cae41b10279bb502a73a7eca136964f3c2
knarr <wayfaringknarr@gmail.com>
2013-11-07 17:43:35 -0500

    Moved main dart file to main.dart, added graph.dart containing DirectedGraph Class

--- graph/web/graph.dart
+++ graph/web/graph.dart
@@ -1,16 +1,61 @@
-import 'dart:html';
+import 'dart:collection';
 
-void main() {
-  querySelector("#sample_text_id")
-    ..text = "Click me!"
-    ..onClick.listen(reverseText);
-}
-
-void reverseText(MouseEvent event) {
-  var text = querySelector("#sample_text_id").text;
-  var buffer = new StringBuffer();
-  for (int i = text.length - 1; i >= 0; i--) {
-    buffer.write(text[i]);
+class DirectedGraph extends IterableBase{
+  final HashMap dGraph = new HashMap();
+  
+  // Adds a new node to the graph.
+  // If the node already exists the graph is unchanged.
+  HashMap addNode(var node) {
+    if (!dGraph.containsKey(node)) {
+      dGraph[node] = new HashSet();
+    }
   }
-  querySelector("#sample_text_id").text = buffer.toString();
+  
+  // Adds an arc from the start node to the dest node.
+  // If the arc already exists the graph is unchanged.
+  // If either endpoint does not exist throws a NoSuchElementException
+  HashMap addEdge(var start, var dest) {
+    if (dGraph.containsKey(start) && dGraph.containsKey(dest)) {
+      dGraph[start].add(dest);
+    } else {
+      throw new NoSuchElementException("Both nodes must be in the graph.");
+    }
+  }
+  
+  // Removes an arc between the start node to the dest node.
+  // If the arc does not exist then returns the Hashmap unchanged.
+  // If either endpoint does not exist throws a NoSuchElementException.
+  HashMap removeEdge(var start, var dest) {
+    if (dGraph.containsKey(start) && dGraph.containsKey(dest)) {
+      dGraph[start].remove(dest);
+    } else {
+      throw new NoSuchElementException("Both nodes must be in the graph.");
+    }
+  }
+  
+  // Returns whether an arc exists from the start node to the end node.
+  // If either endpoint does not exist throws a NoSuchElementExcpetion.
+  bool edgeExists(var start, var end) {
+    if (dGraph.containsKey(start) && dGraph.containsKey(end)) {
+      return dGraph[start].contains(end);
+    } else {
+      throw new NoSuchElementException("Both nodes must be in the graph.");
+    }
+  }
+  
+  // Given a set of nodes in the graph; returns the nodes accessable from that node.
+  // If the node does no exist throws a NoSuchElementException.
+  HashSet edgesFrom(var node) {
+    final HashSet arcs = dGraph[node];
+    if (arcs == null) {
+      throw new NoSuchElementException("Source node does not exist.");
+    } else {
+      return arcs;
+    }
+  }
+  
+  // Returns the iterator over the nodes in the graph.
+  Iterator get iterator => dGraph.keys.iterator;
+  // Returns the number of nodes in the graph
+  int get length => dGraph.length;
 }
